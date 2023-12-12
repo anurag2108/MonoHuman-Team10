c7e7a11f457087c6b81c93e025027530b7b63fdd
anuragnaren2108 <anuragnaren2108@gmail.com>
2023-12-12 16:20:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,122 @@
+# MonoHuman: Animatable Human Neural Field from Monocular Video (CVPR 2023)
+
+<img src="./assets/teaser.jpg">
+
+
+**MonoHuman: Animatable Human Neural Field from Monocular Video**<br>
+[Zhengming Yu](https://yzmblog.github.io/),
+[Wei Cheng](#),
+[Xian Liu](https://alvinliu0.github.io/),
+[Wayne Wu](https://wywu.github.io/),
+and [Kwan-Yee Lin](https://kwanyeelin.github.io/)
+<br>
+**[Demo Video](https://www.youtube.com/watch?v=T91fXw9dOmM)** | **[Project Page](https://yzmblog.github.io/projects/MonoHuman)**
+| **[Paper](https://arxiv.org/abs/2304.02001)**
+
+This is an official implementation of MonoHuman using [PyTorch](https://pytorch.org/)
+
+>Animating virtual avatars with free-view control is crucial for various applications like virtual reality and digital entertainment. Previous studies attempt to utilize the representation power of neural radiance field (NeRF) to reconstruct the human body from monocular videos. Recent works propose to graft a deformation network into the NeRF to further model the dynamics of the human neural field for animating vivid human motions. However, such pipelines either rely on pose-dependent representations or fall short of motion coherency due to frame-independent optimization, making it difficult to generalize to unseen pose sequences realistically. In this paper, we propose a novel framework **MonoHuman**, which robustly renders view-consistent and high-fidelity avatars under arbitrary novel poses. Our key insight is to model the deformation field with bi-directional constraints and explicitly leverage the off-the-peg keyframe information to reason the feature correlations for coherent results. In particular, we first propose a Shared Bidirectional Deformation module, which creates a pose-independent generalizable deformation field by disentangling backward and forward deformation correspondences into shared skeletal motion weight and separate non-rigid motions. Then, we devise a Forward Correspondence Search module, which queries the correspondence feature of keyframes to guide the rendering network. The rendered results are thus multi-view consistent with high fidelity, even under challenging novel pose settings. Extensive experiments demonstrate the superiority of proposed MonoHuman over state-of-the-art methods.
+
+
+
+## Installation
+
+We recommend to use [Anaconda](https://www.anaconda.com/).
+
+Create and activate a virtual environment.
+
+    conda env create -f environment.yaml
+    conda activate Monohuman
+
+### `Download SMPL model`
+
+Download the gender neutral SMPL model from [here](https://smplify.is.tue.mpg.de/), and unpack **mpips_smplify_public_v2.zip**.
+
+Copy the smpl model.
+
+    cp /path/to/smpl/smplify_public/code/models/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl third_parties/smpl/models
+
+Follow [this page](https://github.com/vchoutas/smplx/tree/master/tools) to remove Chumpy objects from the SMPL model.
+
+## Run on ZJU-Mocap Dataset
+
+### `Prepare a dataset`
+
+1. Download ZJU-Mocap dataset from [here](https://github.com/zju3dv/neuralbody/blob/master/INSTALL.md#zju-mocap-dataset). 
+
+2. Modify the yaml file of subject at `tools/prepare_zju_mocap/xxx.yaml` as below (Replace the 'xxx' to the subject ID):
+```
+    dataset:
+        zju_mocap_path: /path/to/zju_mocap
+        subject: 'xxx'
+        sex: 'neutral'
+
+...
+```
+3. Run the data preprocessing script.
+```
+    cd tools/prepare_zju_mocap
+    python prepare_dataset.py --cfg xxx.yaml
+    cd ../../
+```
+
+4. Modify the 'dataset_path' in core/data/dataset_args.py to your /path/to/dataset
+
+### `Training`
+Please replace the 'xxx' to the subject ID
+
+    python train.py --cfg configs/monohuman/zju_mocap/xxx/xxx.yaml resume False
+
+
+### `Rendering and Evalutaion`
+
+Render the motion sequence. (e.g., subject 377)
+
+    python run.py \
+        --type movement \
+        --cfg configs/monohuman/zju_mocap/377/377.yaml 
+
+![video](assets/377_movement.gif)
+
+Render free-viewpoint images on a particular frame (e.g., subject 386 and frame 100).
+
+    python run.py \
+        --type freeview \
+        --cfg configs/monohuman/zju_mocap/386/386.yaml \
+        freeview.frame_idx 100
+![video](assets/386_free.gif)
+
+Render the text driven motion sequence.
+Generate poses sequence from [MDM](https://github.com/GuyTevet/motion-diffusion-model), and put the sequence to `path/to/pose_sequence/sequence.npy` (e.g., subject 394 and backflip)
+
+    python run.py \
+        --type text \
+        --cfg configs/monohuman/zju_mocap/394/394.yaml \
+        text.pose_path path/to/pose_sequence/backflip.npy
+![video](assets/backflip.gif)
+
+## Acknowledgement
+
+Our code took reference from [HumanNeRF](https://github.com/chungyiweng/humannerf), [IBRNet](https://github.com/googleinterns/IBRNet), [Neural Body](https://github.com/zju3dv/neuralbody). We thank these authors for their great works and open-source contribution.
+
+## TODO
+- [x] Code Release.
+- [x] Demo Video Release.
+- [x] Paper Release.
+- [ ] DDP Training.
+- [ ] Pretrained Model Release.
+
+
+<a name="citation"></a>
+## Citation
+If you find this work useful for your research, please consider citing our paper: 
+
+```bibtex
+@inproceedings{yu2023monohuman,
+  title={{MonoHuman}: Animatable Human Neural Field from Monocular Video},
+  author={Yu, Zhengming and Cheng, Wei and Liu, xian and Wu, Wayne and Lin, Kwan-Yee},
+  booktitle={CVPR},
+  year={2023}
+}
+```
+
---
+++ core/utils/transforms.py
@@ -0,0 +1,232 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# https://pytorch3d.readthedocs.io/en/latest/_modules/pytorch3d/transforms/rotation_conversions.html#matrix_to_quaternion
+
+import torch
+import torch.nn.functional as F
+
+def _copysign(a, b):
+    """
+    Return a tensor where each element has the absolute value taken from the,
+    corresponding element of a, with sign taken from the corresponding
+    element of b. This is like the standard copysign floating-point operation,
+    but is not careful about negative 0 and NaN.
+    Args:
+        a: source tensor.
+        b: tensor whose signs will be used, of the same shape as a.
+    Returns:
+        Tensor of the same shape as a with the signs of b.
+    """
+    signs_differ = (a < 0) != (b < 0)
+    return torch.where(signs_differ, -a, a)
+
+def _sqrt_positive_part(x):
+    """
+    Returns torch.sqrt(torch.max(0, x))
+    but with a zero subgradient where x is 0.
+    """
+    ret = torch.zeros_like(x)
+    positive_mask = x > 0
+    ret[positive_mask] = torch.sqrt(x[positive_mask])
+    return ret
+
+def matrix_to_quaternion(matrix):
+    """
+    Convert rotations given as rotation matrices to quaternions.
+    Args:
+        matrix: Rotation matrices as tensor of shape (..., 3, 3).
+    Returns:
+        quaternions with real part first, as tensor of shape (..., 4).
+    """
+    if matrix.size(-1) != 3 or matrix.size(-2) != 3:
+        raise ValueError(f"Invalid rotation matrix  shape f{matrix.shape}.")
+    m00 = matrix[..., 0, 0]
+    m11 = matrix[..., 1, 1]
+    m22 = matrix[..., 2, 2]
+    o0 = 0.5 * _sqrt_positive_part(1 + m00 + m11 + m22)
+    x = 0.5 * _sqrt_positive_part(1 + m00 - m11 - m22)
+    y = 0.5 * _sqrt_positive_part(1 - m00 + m11 - m22)
+    z = 0.5 * _sqrt_positive_part(1 - m00 - m11 + m22)
+    o1 = _copysign(x, matrix[..., 2, 1] - matrix[..., 1, 2])
+    o2 = _copysign(y, matrix[..., 0, 2] - matrix[..., 2, 0])
+    o3 = _copysign(z, matrix[..., 1, 0] - matrix[..., 0, 1])
+
+    return torch.stack((o0, o1, o2, o3), -1)
+
+def quaternion_to_axis_angle(quaternions):
+    """
+    Convert rotations given as quaternions to axis/angle.
+    Args:
+        quaternions: quaternions with real part first,
+            as tensor of shape (..., 4).
+    Returns:
+        Rotations given as a vector in axis angle form, as a tensor
+            of shape (..., 3), where the magnitude is the angle
+            turned anticlockwise in radians around the vector's
+            direction.
+    """
+    norms = torch.norm(quaternions[..., 1:], p=2, dim=-1, keepdim=True)
+    half_angles = torch.atan2(norms, quaternions[..., :1])
+    angles = 2 * half_angles
+    eps = 1e-6
+    small_angles = angles.abs() < eps
+    sin_half_angles_over_angles = torch.empty_like(angles)
+    sin_half_angles_over_angles[~small_angles] = (
+        torch.sin(half_angles[~small_angles]) / angles[~small_angles]
+    )
+    # for x small, sin(x/2) is about x/2 - (x/2)^3/6
+    # so sin(x/2)/x is about 1/2 - (x*x)/48
+    sin_half_angles_over_angles[small_angles] = (
+        0.5 - (angles[small_angles] * angles[small_angles]) / 48
+    )
+
+    return quaternions[..., 1:] / sin_half_angles_over_angles
+
+
+def rotation_6d_to_matrix(d6):
+    """ 
+    Converts 6D rotation representation by Zhou et al. [1] to rotation matrix 
+    using Gram--Schmidt orthogonalisation per Section B of [1]. 
+    Args: 
+        d6: 6D rotation representation, of size (*, 6) 
+
+    Returns: 
+        batch of rotation matrices of size (*, 3, 3) 
+
+    [1] Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H. 
+    On the Continuity of Rotation Representations in Neural Networks. 
+    IEEE Conference on Computer Vision and Pattern Recognition, 2019. 
+    Retrieved from http://arxiv.org/abs/1812.07035 
+    """ 
+
+    a1, a2 = d6[..., :3], d6[..., 3:] 
+    b1 = F.normalize(a1, dim=-1) 
+    b2 = a2 - (b1 * a2).sum(-1, keepdim=True) * b1 
+    b2 = F.normalize(b2, dim=-1) 
+    b3 = torch.cross(b1, b2, dim=-1)
+
+    return torch.stack((b1, b2, b3), dim=-2) 
+
+def matrix_to_axis_angle(matrix):
+    """ 
+    Convert rotations given as rotation matrices to axis/angle. 
+
+    Args: 
+        matrix: Rotation matrices as tensor of shape (..., 3, 3). 
+
+    Returns: 
+        Rotations given as a vector in axis angle form, as a tensor 
+            of shape (..., 3), where the magnitude is the angle 
+            turned anticlockwise in radians around the vector's 
+            direction. 
+    """
+
+    return quaternion_to_axis_angle(matrix_to_quaternion(matrix)) 
+
+def matrix_to_rotation_6d(matrix: torch.Tensor) -> torch.Tensor:
+    """
+    Converts rotation matrices to 6D rotation representation by Zhou et al. [1]
+    by dropping the last row. Note that 6D representation is not unique.
+    Args:
+        matrix: batch of rotation matrices of size (*, 3, 3)
+
+    Returns:
+        6D rotation representation, of size (*, 6)
+
+    [1] Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H.
+    On the Continuity of Rotation Representations in Neural Networks.
+    IEEE Conference on Computer Vision and Pattern Recognition, 2019.
+    Retrieved from http://arxiv.org/abs/1812.07035
+    """
+    batch_dim = matrix.size()[:-2]
+    return matrix[..., :2, :].clone().reshape(batch_dim + (6,))
+
+def axis_angle_to_matrix(axis_angle: torch.Tensor) -> torch.Tensor:
+    """
+    Convert rotations given as axis/angle to rotation matrices.
+
+    Args:
+        axis_angle: Rotations given as a vector in axis angle form,
+            as a tensor of shape (..., 3), where the magnitude is
+            the angle turned anticlockwise in radians around the
+            vector's direction.
+
+    Returns:
+        Rotation matrices as tensor of shape (..., 3, 3).
+    """
+    return quaternion_to_matrix(axis_angle_to_quaternion(axis_angle))
+
+
+def axis_angle_to_quaternion(axis_angle: torch.Tensor) -> torch.Tensor:
+    """
+    Convert rotations given as axis/angle to quaternions.
+
+    Args:
+        axis_angle: Rotations given as a vector in axis angle form,
+            as a tensor of shape (..., 3), where the magnitude is
+            the angle turned anticlockwise in radians around the
+            vector's direction.
+
+    Returns:
+        quaternions with real part first, as tensor of shape (..., 4).
+    """
+    angles = torch.norm(axis_angle, p=2, dim=-1, keepdim=True)
+    half_angles = angles * 0.5
+    eps = 1e-6
+    small_angles = angles.abs() < eps
+    sin_half_angles_over_angles = torch.empty_like(angles)
+    sin_half_angles_over_angles[~small_angles] = (
+        torch.sin(half_angles[~small_angles]) / angles[~small_angles]
+    )
+    # for x small, sin(x/2) is about x/2 - (x/2)^3/6
+    # so sin(x/2)/x is about 1/2 - (x*x)/48
+    sin_half_angles_over_angles[small_angles] = (
+        0.5 - (angles[small_angles] * angles[small_angles]) / 48
+    )
+    quaternions = torch.cat(
+        [torch.cos(half_angles), axis_angle * sin_half_angles_over_angles],
+        dim=-1,
+    )
+    return quaternions
+
+
+def quaternion_to_matrix(quaternions: torch.Tensor) -> torch.Tensor:
+    """
+    Convert rotations given as quaternions to rotation matrices.
+
+    Args:
+        quaternions: quaternions with real part first,
+            as tensor of shape (..., 4).
+
+    Returns:
+        Rotation matrices as tensor of shape (..., 3, 3).
+    """
+    r, i, j, k = torch.unbind(quaternions, -1)
+    two_s = 2.0 / (quaternions * quaternions).sum(-1)
+
+    o = torch.stack(
+        (
+            1 - two_s * (j * j + k * k),
+            two_s * (i * j - k * r),
+            two_s * (i * k + j * r),
+            two_s * (i * j + k * r),
+            1 - two_s * (i * i + k * k),
+            two_s * (j * k - i * r),
+            two_s * (i * k - j * r),
+            two_s * (j * k + i * r),
+            1 - two_s * (i * i + j * j),
+        ),
+        -1,
+    )
+    return o.reshape(quaternions.shape[:-1] + (3, 3))
+
+
+def _sqrt_positive_part(x: torch.Tensor) -> torch.Tensor:
+    """
+    Returns torch.sqrt(torch.max(0, x))
+    but with a zero subgradient where x is 0.
+    """
+    ret = torch.zeros_like(x)
+    positive_mask = x > 0
+    ret[positive_mask] = torch.sqrt(x[positive_mask])
+    return ret
+
